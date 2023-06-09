d49ed678069b80445ae454bc863824937cb3ddc9
BarbolinPavel <frost1449@gmail.com>
2023-06-09 23:21:02 +0300

    add test

--- src/test/java/GitHubTests.java
+++ src/test/java/GitHubTests.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTests {
@@ -9,12 +10,16 @@ public class GitHubTests {
         //Открываем GitHub
         open ("https://github.com/");
 
-        //Вводим в поле поиска "selenide"
-        $("[name=\"q\"]").setValue("selenide").pressEnter();
-
+        $("[name='q']").click();
+        $("[name='q']").setValue("selenide").pressEnter();
         $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
 
+        $(".markdown-body").shouldHave(text("Welcome to the selenide wiki"));
 
-        sleep(5000);
+        $("li.wiki-more-pages-link [type='button']").click();
+        $("ul li.wiki-more-pages [href='/selenide/selenide/wiki/SoftAssertions']").click();
+
+        $("#user-content-3-using-junit5-extend-test-class").ancestor("h4").shouldHave(text("JUnit5 extend test class"));
     }
 }
